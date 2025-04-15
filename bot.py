7e349c9e61da17f9764f80319ddbd31076db7b49
AppLab📱 <083molonlabe@gmail.com>
2025-04-15 18:17:24 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 # ------------------------- Настройки -------------------------
 TOKEN = "7633660729:AAEF7FnE9HO0jfBsJXHRTOznP0s3jiwntPs"
-OWNER_ID = 204541083
+OWNER_ID = 2045410830
 CHANNEL_ID = "@your_channel"  # Замените на нужный ID или @username канала
 # -------------------------------------------------------------
 
